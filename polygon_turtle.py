1d69e3dea826ff2967efc9c444e5f35460bea20c
nazcat <nazli.uremek@gmail.com>
2021-07-02 11:05:53 -0400

    Update polygon_turtle.py

--- polygon_turtle.py
+++ polygon_turtle.py
@@ -1,3 +1,5 @@
+# create a polygon
+
 import turtle
 
 sides = int(input("Enter number of sides"))
@@ -19,3 +21,40 @@ for i in range(sides):
     naz.left(360/sides)
 
 naz.end_fill()
+
+
+# create a smiley
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightblue")
+
+naz = turtle.Turtle()
+naz.color("purple")
+
+# smiley eyes
+naz.penup()
+naz.forward(50)
+naz.pendown()
+naz.left(90)
+naz.forward(50)
+naz.penup()
+naz.left(90)
+naz.forward(100)
+naz.pendown()
+naz.left(90)
+naz.forward(50)
+
+# smiley mouth
+naz.penup()
+naz.right(90)
+naz.forward(50)
+naz.left(90)
+naz.forward(50)
+naz.pendown()
+naz.left(45)
+naz.forward(50)
+naz.left(45)
+naz.forward(130)
+naz.left(45)
+naz.forward(50)
