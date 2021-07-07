de367ff918f3a711b3aa1ed9367ffc5cef3466b1
nazcat <nazli.uremek@gmail.com>
2021-07-06 23:27:59 -0400

    Create using_format_in_string.py

---
+++ using_format_in_string.py
@@ -0,0 +1,16 @@
+# original code
+
+scores = [("Rodney Dangerfield", -1), ("Marlon Brando", 1), ("You", 100)]
+for person in scores:
+    name = person[0]
+    score = person[1]
+    print("Hello " + name + ". Your score is " + str(score))
+
+
+# better code using format
+
+scores = [("Rodney Dangerfield", -1), ("Marlon Brando", 1), ("You", 100)]
+for person in scores:
+    name = person[0]
+    score = person[1]
+    print("Hello {}. Your score is {}.".format(name, score))
