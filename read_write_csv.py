fc668578f96295fd2469300e14a29267c7de8ea1
nazcat <nazli.uremek@gmail.com>
2021-07-08 14:21:09 -0400

    Update read_write_csv.py

--- read_write_csv.py
+++ read_write_csv.py
@@ -11,3 +11,19 @@ for row in lines[1:]:
         print("{}: {}; {}".format(vals[0],vals[4],vals[5]))
 
 # write csv file
+
+olympians = [("John Aalberg", 31, "Cross Country Skiing"),
+             ("Minna Maarit Aalto", 30, "Sailing"),
+             ("Win Valdemar Aaltonen", 54, "Art Competitions"),
+             ("Wakako Abe", 18, "Cycling")]
+
+outfile = open("reduced_olympics.csv", "w")
+# output the header row
+outfile.write('Name,Age,Sport')
+outfile.write('\n')
+# output each of the rows:
+for olympian in olympians:
+    row_string = '{},{},{}'.format(olympian[0], olympian[1], olympian[2])
+    outfile.write(row_string)
+    outfile.write('\n')
+outfile.close()
