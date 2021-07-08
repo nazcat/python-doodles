e10953181d4bb00e8a46325971d7c982662b55a8
nazcat <nazli.uremek@gmail.com>
2021-07-08 14:28:13 -0400

    Update read_write_csv.py

--- read_write_csv.py
+++ read_write_csv.py
@@ -24,7 +24,7 @@ outfile.write('\n')
 # output each of the rows:
 for olympian in olympians:
     row_string = '{},{},{}'.format(olympian[0], olympian[1], olympian[2])
-    # row_string = ','.join(olympian[0], olympian[1], olympian[2])
+    # row_string = ','.join([olympian[0], str(olympian[1]), olympian[2]])
     outfile.write(row_string)
     outfile.write('\n')
 outfile.close()
